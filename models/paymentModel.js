cab8548c6059819782de936a36f3f60e5939ab7b
Aya Ahmed Elrayes <ayaahmedelrayes@gmail.com>
2026-05-21 11:25:44 +0300

    feat: create payment schema with Joi validation

---
+++ models/paymentModel.js
@@ -0,0 +1,13 @@
+const mongoose=require ('mongoose')
+const paymentSchema= new mongoose.Schema({
+    userId:{type:mongoose.Schema.Types.ObjectId, ref :'User',required:true},
+    amount:{type:Number, required:true},
+    cardHolder: { type: String },
+cardLast4: { type: String },
+expiryDate: { type: String },
+status: {type: String, enum:['SUCCESS', 'FAILED', 'PENDING'], default:'PENDING'},
+purpose: { type: String, enum: ['BRAND_SUBSCRIPTION', 'BAZAAR_SUBSCRIPTION', 'BRAND_ENTRY_FEE','ORDER_CHECKOUT'] },
+transactionId:{type:String, unique: true, sparse: true}
+
+},{timestamps:true})
+module.exports=mongoose.model('Payment',paymentSchema)
