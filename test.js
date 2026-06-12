5bf3295ebc0ec1905188faf86f3523d0679e41de
Maram_elsaed <maramghozal@gmail.com>
2026-06-12 21:15:30 +0300

    chore: remove unused files

--- test.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const generateToken=require('./utils/generateWebToken');
-const tokens = generateToken({
-  id: "6a1dbec0f8cf15a70d660886",
-  role: "BAZAAR_OWNER",
-});
-
-console.log(tokens);
--- testoken.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const generateToken=require('./utils/generateWebToken');
-const tokens = generateToken({
-  id: "6a1dbec0f8cf15a70d66088a",
-  role: "BRAND_OWNER",
-});
-
-console.log(tokens);
