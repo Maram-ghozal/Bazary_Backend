99bd93500037f5faa788b61ad51072f47d25ebd2
MariamAboslema <mariamaboslema8@gmail.com>
2026-06-18 09:34:50 +0300

    fix: add missing email and brandType fields in Brand.create()

--- controller/bazaarControllers.js
+++ controller/bazaarControllers.js
@@ -860,7 +860,7 @@ const approveBrand = asyncWrapper(async (req, res, next) => {
             metadata: { waitingListId: entry._id.toString() }
         });
 
-        entry.paymentLink = `${process.env.FRONTEND_URL}/payment/${clientSecret}`;
+        entry.paymentLink = `http://localhost:3000/payment/${clientSecret}`;
         await entry.save();
 
         await sendEmail({
