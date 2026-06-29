eb97d8a5e1bed047575eb42cfab973714bf83f5d
MariamAboslema <mariamaboslema8@gmail.com>
2026-06-29 14:59:05 +0300

    feat: implement complete wishlist functionality

---
+++ models/WishlistModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const wishlistItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  brandId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Brand',
+    required: true,
+  },
+  bazaarId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Bazaar',
+    required: true,
+  },
+}, { _id: false });
+
+const wishlistSchema = new mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    default: null,
+  },
+  guestId: {
+    type: String,
+    default: null,
+  },
+  items: [wishlistItemSchema],
+}, { timestamps: true });
+
+wishlistSchema.index({ customerId: 1 }, { unique: true, sparse: true });
+wishlistSchema.index({ guestId: 1 },   { unique: true, sparse: true });
+
+module.exports = mongoose.model('Wishlist', wishlistSchema);
